8b45bc00729cacef418b1925e8774dbf8373455c
Eric Erhardt <eric.erhardt@microsoft.com>
2020-06-30 15:10:28 -0500

    Add project files.

---
+++ ConsoleApp97/Program.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp97
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var supportedWriter = new LoggingTextWriter(ConsoleFormatInfo.AnsiSupportedInfo);
+            var notSupportedWriter = new LoggingTextWriter(ConsoleFormatInfo.AnsiNotSupportedInfo);
+
+            var consoleLogFormatter = new PrettyConsoleLogFormatter();
+            consoleLogFormatter.Write<object>(LogLevel.Information, "CA100", default, new object(), null, (o, e) => "state string result", null, supportedWriter);
+            Console.WriteLine();
+            
+            consoleLogFormatter.Write<object>(LogLevel.Information, "CA100", default, new object(), null, (o, e) => "state string result", null, notSupportedWriter);
+            Console.WriteLine();
+
+            // test writing to the console directly
+            Console.WriteLine($"{Ansi.Color.Foreground.Green}Console test{Ansi.Color.Foreground.Default}");
+        }
+    }
+
+    public partial interface IConsoleLogFormatter
+    {
+        string Name { get; }
+
+        void Write<TState>(LogLevel logLevel, string category, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter, IExternalScopeProvider scopeProvider, TextWriter textWriter);
+    }
+
+    public class PrettyConsoleLogFormatter : IConsoleLogFormatter
+    {
+        public string Name => "Pretty";
+
+        public void Write<TState>(LogLevel logLevel, string category, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter, IExternalScopeProvider scopeProvider, TextWriter writer)
+        {
+            writer.Write(Ansi.Color.Foreground.Red);
+            writer.Write(logLevel);
+
+            writer.Write(Ansi.Color.Foreground.White);
+            writer.Write(category);
+
+            writer.Write(Ansi.Color.Foreground.Blue);
+            writer.Write(formatter.Invoke(state, exception));
+
+            writer.Write(Ansi.Color.Foreground.Cyan);
+            writer.Write(4.5f);
+
+            writer.Write(Ansi.Color.Foreground.Default);
+
+            writer.WriteFormat($"{Ansi.Color.Foreground.LightMagenta}John test{Ansi.Color.Foreground.Default}");
+        }
+    }
+
+    public static class TextWriterExtensions
+    {
+        public static void WriteFormat(this TextWriter writer, FormattableString s)
+        {
+            writer.Write(s.ToString(writer.FormatProvider));
+            //switch (s.ArgumentCount)
+            //{
+            //    case 0: writer.Write(s.Format); break;
+            //    case 1: writer.Write(s.Format, Format(s.GetArgument(0))); break;
+            //    case 2: writer.Write(s.Format, Format(s.GetArgument(0)), Format(s.GetArgument(1)));break;
+            //    case 3: writer.Write(s.Format, Format(s.GetArgument(0)), Format(s.GetArgument(1)), Format(s.GetArgument(2)));break;
+            //    default:
+            //        object?[] args = s.GetArguments();
+            //        object[] formattedArgs = new object[args.Length];
+            //        for (int i = 0; i < args.Length; i++)
+            //        {
+            //            formattedArgs[i] = Format(args[i]);
+            //        }
+            //        writer.Write(s.Format, formattedArgs);
+            //        break;
+            //}
+
+
+        }
+    }
+
+    class LoggingTextWriter : TextWriter
+    {
+        //private int _index;
+        /// <summary>
+        /// This should come from ArrayPool
+        /// </summary>
+        //private char[] _buffer = new char[512];
+
+        public LoggingTextWriter(IFormatProvider formatProvider)
+            : base(formatProvider)
+        {
+
+        }
+
+        public override Encoding Encoding => Console.Out.Encoding;
+
+        public override void Write(char value)
+        {
+            Console.Out.Write(value);
+
+           // _buffer[_index] = value;
+            //_index++;
+        }
+    }
+}
---
+++ ConsoleApp97/System.Console/ConsoleFormatInfo.cs
@@ -0,0 +1,65 @@
+﻿namespace System
+{
+    public sealed class ConsoleFormatInfo : IFormatProvider
+    {
+        private static ConsoleFormatInfo s_ansiSupportedInfo;
+        private static ConsoleFormatInfo s_ansiNotSupportedInfo;
+        private bool _isReadOnly;
+        private bool _supportsAnsiCodes;
+
+        private ConsoleFormatInfo()
+        {
+        }
+
+        public static ConsoleFormatInfo CurrentInfo
+        {
+            get
+            {
+                // TODO: read this from the Console
+                return AnsiSupportedInfo;
+            }
+        }
+
+        public static ConsoleFormatInfo AnsiSupportedInfo => s_ansiSupportedInfo ??=
+            new ConsoleFormatInfo { _isReadOnly = true, _supportsAnsiCodes = true };
+
+        public static ConsoleFormatInfo AnsiNotSupportedInfo => s_ansiNotSupportedInfo ??=
+            new ConsoleFormatInfo { _isReadOnly = true, _supportsAnsiCodes = false };
+
+        public bool SupportsAnsiCodes
+        {
+            get => _supportsAnsiCodes;
+            set
+            {
+                VerifyWritable();
+                _supportsAnsiCodes = value;
+            }
+        }
+
+        public static ConsoleFormatInfo GetInstance(IFormatProvider formatProvider)
+        {
+            return formatProvider == null ?
+                CurrentInfo : // Fast path for a null provider
+                GetProviderNonNull(formatProvider);
+
+            static ConsoleFormatInfo GetProviderNonNull(IFormatProvider provider)
+            {
+                return
+                    provider as ConsoleFormatInfo ?? // Fast path for an CFI
+                    provider.GetFormat(typeof(ConsoleFormatInfo)) as ConsoleFormatInfo ??
+                    CurrentInfo;
+            }
+        }
+
+        public object GetFormat(Type formatType) =>
+            formatType == typeof(ConsoleFormatInfo) ? this : null;
+
+        private void VerifyWritable()
+        {
+            if (_isReadOnly)
+            {
+                throw new InvalidOperationException("ReadOnly");
+            }
+        }
+    }
+}
